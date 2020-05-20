bb14027a0a1eb0c5de03a921b186540d4fc9c3e2
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-20 17:31:55 +0900

    added sessions controller and used omniauth tutorial language

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,25 @@ class User < ApplicationRecord
 end
 
 # :registerable is for signing up; might want to disable for RAILS_GOT...
+
+# EXAMPLE FROM OMNIAUTH TUTORIAL ...
+
+# class User < ApplicationRecord
+#   has_many :wines
+
+#   validates :user_name, :email, presence: true
+#   validates :user_name, :email, uniqueness: true
+#   has_secure_password  #password validations
+
+#   def self.from_omniauth(auth)
+#     #Creates a new user only if it doesn't exist
+#     where(email: auth.info.email).first_or_initialize do |user|
+#       user.user_name = auth.info.name
+#       user.email = auth.info.email
+#       if auth.info.uid
+#         user.uid = auth.info.uid
+#       end
+#       user.password = SecureRandom.hex
+#     end
+#   end
+# end
