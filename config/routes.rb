d45b91f1a8b5dc2642365e7afe11f6b2f5ffd50d
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-05-20 18:03:51 +0900

    generated omniauth controller and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  # replacing original devise line with omniauth specifications
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+
   root to: 'grants#index'
 
   get 'grants/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/auth/:provider/callback' => 'sessions#omniauth'
 end
